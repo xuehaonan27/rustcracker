5c892243aef10130767e5963e548d8c2e0f41b09
Xue Haonan <xuehaonan27@gmail.com>
2024-02-16 00:23:13 +0800

    feat: modified benchmark.rs, adding network set.

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -3,6 +3,7 @@
 use std::path::PathBuf;
 
 use log::{error, info};
+use run_script::ScriptOptions;
 use rustcracker::{
     components::{
         command_builder::VMMCommandBuilder,
@@ -29,12 +30,47 @@ const RESOURCE_DIR: &'static str = "/tmp/rustcracker/res";
 const SNAPSHOT_DIR: &'static str = "/tmp/rustcracker/snapshot";
 // a tokio coroutine that might be parallel with other coroutines
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), MachineError> {
     // Initialize env logger
     let _ = env_logger::builder().is_test(false).try_init();
 
     // check that kvm is accessible
     check_kvm()?;
+
+    // set up network
+    let (code, _output, error) = run_script::run_script!(
+        r#"
+        TAP_DEV="tap$1"
+        HOST_IFACE="eth$1"
+        TAP_IP="172.16.0.1"
+        MASK_SHORT="/30"
+
+        # Setup network interface
+        sudo ip link del "$TAP_DEV" 2> /dev/null || true
+        sudo ip tuntap add dev "$TAP_DEV" mode tap
+        sudo ip addr add "${TAP_IP}${MASK_SHORT}" dev "$TAP_DEV"
+        sudo ip link set dev "$TAP_DEV" up
+
+        # Enable ip forwarding
+        sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+        # Set up microVM internet access
+        sudo iptables -t nat -D POSTROUTING -o "$HOST_IFACE" -j MASQUERADE || true
+        sudo iptables -D FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT \
+            || true
+        sudo iptables -D FORWARD -i "$TAP_DEV" -o "$HOST_IFACE" -j ACCEPT || true
+        sudo iptables -t nat -A POSTROUTING -o "$HOST_IFACE" -j MASQUERADE
+        sudo iptables -I FORWARD 1 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+        sudo iptables -I FORWARD 1 -i "$TAP_DEV" -o "$HOST_IFACE" -j ACCEPT
+        "#,
+        &vec![format!("benchmark")],
+        &ScriptOptions::new()
+    ).unwrap();
+    // if networking is configured successfully, then run the machine `name{id}``
+    if !(code == 0 && error == "") {
+        error!(target: "main", "Fail to set up network");
+        return Err(MachineError::Execute(format!("Fail to set up network: {}",error)));
+    }
 
     /* below are configurations that could be transmitted with json file (Serializable and Deserializable) */
 
