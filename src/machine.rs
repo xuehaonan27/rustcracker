9c8bffbdc487460f0e9a14f076dc9d9229f0d385
Xue Haonan <xuehaonan27@gmail.com>
2024-05-24 02:57:02 +0800

    feat: implemented sync machine.

--- src/machine.rs
+++ src/machine.rs
@@ -1,11 +1,104 @@
-mod machine {}
+pub mod machine {
+    use std::io::{BufRead, Write};
+
+    use crate::{
+        events::events,
+        models::{
+            instance_action_info::{ActionType, InstanceActionInfo},
+            snapshot_create_params::{SnapshotCreateParams, SnapshotType},
+            vm,
+        },
+        rtck::Rtck,
+        RtckError, RtckErrorClass, RtckResult,
+    };
+
+    pub struct Machine<S> {
+        rtck: Rtck<S>,
+    }
+
+    impl<S: BufRead + Write> Machine<S> {
+        pub fn create() -> RtckResult<Self> {
+            todo!()
+        }
+
+        pub fn pint_remote(&mut self) -> RtckResult<()> {
+            let mut get_firecracker_version = events::GetFirecrackerVersion::new();
+            Ok(self
+                .rtck
+                .execute(&mut get_firecracker_version)
+                .map_err(|e| {
+                    RtckError::new(
+                        RtckErrorClass::RemoteError,
+                        format!("Fail to ping remote {}", e.to_string()),
+                    )
+                })?)
+        }
+
+        pub fn start(&mut self) -> RtckResult<()> {
+            let mut start_machine = events::CreateSyncAction::new(InstanceActionInfo {
+                action_type: ActionType::InstanceStart,
+            });
+
+            self.rtck.execute(&mut start_machine)?;
+            Ok(())
+        }
+
+        pub fn pause(&mut self) -> RtckResult<()> {
+            let mut pause_machine = events::PatchVm::new(vm::Vm {
+                state: vm::State::Paused,
+            });
+
+            self.rtck.execute(&mut pause_machine)?;
+            Ok(())
+        }
+
+        pub fn resume(&mut self) -> RtckResult<()> {
+            let mut resume_machine = events::PatchVm::new(vm::Vm {
+                state: vm::State::Resumed,
+            });
+
+            self.rtck.execute(&mut resume_machine)?;
+            Ok(())
+        }
+
+        pub async fn stop(&mut self) -> RtckResult<()> {
+            let mut stop_machine = events::CreateSyncAction::new(InstanceActionInfo {
+                action_type: ActionType::SendCtrlAtlDel,
+            });
+
+            self.rtck.execute(&mut stop_machine)?;
+            Ok(())
+        }
+
+        pub fn delete(&mut self) -> RtckResult<()> {
+            todo!()
+        }
+
+        pub fn snapshot<P: AsRef<str>, Q: AsRef<str>>(
+            &mut self,
+            state_path: P,
+            mem_path: Q,
+            _type: SnapshotType,
+        ) -> RtckResult<()> {
+            let mut create_snapshot = events::CreateSnapshot::new(SnapshotCreateParams {
+                mem_file_path: state_path.as_ref().to_string(),
+                snapshot_path: mem_path.as_ref().to_string(),
+                snapshot_type: Some(_type),
+                version: None,
+            });
+
+            self.rtck.execute(&mut create_snapshot)?;
+            Ok(())
+        }
+    }
+}
 
 pub mod machine_async {
     use parking_lot::Mutex;
     use tokio::io::{AsyncBufRead, AsyncWrite};
 
     use crate::{
-        events::events_async::{self, GetFirecrackerVersion},
+        events::events_async,
         models::{
             instance_action_info::{ActionType, InstanceActionInfo},
             snapshot_create_params::{SnapshotCreateParams, SnapshotType},
@@ -26,8 +119,8 @@ pub mod machine_async {
         }
 
         /// Ping firecracker to check its soundness
-        pub async fn ping_remote(&mut self) -> RtckResult<()> {
-            let get_firecracker_version = GetFirecrackerVersion::new();
+        pub async fn ping_remote(&self) -> RtckResult<()> {
+            let get_firecracker_version = events_async::GetFirecrackerVersion::new();
             Ok(self
                 .rtck
                 .lock()
